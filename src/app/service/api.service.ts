464c39148073576c145cb42591ba8f42afbd90fa
null <null>
2024-04-05 16:31:24 -0300

    Alteração no service

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -5,11 +5,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
-  private url: string = "https://api.harvardartmuseums.org/object?apikey=ec3952b0-5b72-488a-8e66-207b556cd15b&"; // URL da API com a chave de acesso
-  constructor(private http: HttpClient) {} // Construtor da classe ApiService, injetando o serviço HttpClient
+  private Chaveacesso: string;
+  private url: string = "https://api.harvardartmuseums.org/"; // URL da API com a chave de acesso
+  constructor(private http: HttpClient) {
+
+    this.Chaveacesso = "apikey=ec3952b0-5b72-488a-8e66-207b556cd15b";
+
+  } // Construtor da classe ApiService, injetando o serviço HttpClient
 
   getData() {
     // Método para obter dados da API
-    return this.http.get<any>(`${this.url}page=1`); // Retorna a resposta de uma solicitação GET para a URL da API
+    return this.http.get<any>(`${this.url}`); // Retorna a resposta de uma solicitação GET para a URL da API
   }
 }
